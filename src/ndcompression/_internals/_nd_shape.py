ca7928bad3675667ae20fb107ab4899ac21303b2
Wannes Vantorre <vantorrewannes@gmail.com>
2025-03-08 10:06:30 +0100

    feat: Implement NDShape Slices method

--- src/ndcompression/_internals/_nd_shape.py
+++ src/ndcompression/_internals/_nd_shape.py
@@ -1,4 +1,6 @@
+import itertools
 import math
+
 
 class NDShape:
     def __init__(self, shape: list[int]):
@@ -25,6 +27,37 @@ class NDShape:
             indices.append(remaining // stride)
             remaining %= stride
         return indices
+
+    def slices(self):
+        rows_template = self.__rows_template()
+        for shape_index, length in enumerate(self.shape):
+            num_rows = math.prod(self.shape) // length
+            for row in itertools.islice(rows_template, num_rows):
+                for length_index, indecies in enumerate(row):
+                    indecies = list(indecies)
+                    indecies[shape_index] = length_index
+                    row[length_index] = tuple(indecies)
+                yield row
+
+    def __trimmed_shape(self):
+        for shape_index in range(len(self.shape)):
+            yield [
+                1 if i == shape_index else self.shape[i] for i in range(len(self.shape))
+            ]
+
+    def __column_parts(self):
+        for column in self.__trimmed_shape():
+            yield itertools.product(*[range(length) for length in column])
+
+    def __column(self):
+        for column in itertools.chain(*self.__column_parts()):
+            yield tuple(column)
+
+    def __rows_template(self):
+        column = self.__column()
+        for length in self.shape:
+            for _ in range((math.prod(self.shape) // length)):
+                yield list(itertools.repeat(next(column), length))
 
     def __len__(self):
         return self.len
